229f53fb80466398051c6b29ea56eafc5b66004f
Sam Layton <slayton@uccs.edu>
2019-11-03 12:09:07 -0700

    Fixed bug where new branches would spawn as children of starting trunk

--- FractalSynesthesia/Assets/Scripts/TreeGenerator.cs
+++ FractalSynesthesia/Assets/Scripts/TreeGenerator.cs
@@ -55,13 +55,10 @@ public class TreeGenerator : MonoBehaviour
         // while generation has not exceeded limit
         while (generation <= maxGenerations)
         {
-            Debug.Log(generation);
-
             // initialize list storing branches created from this generation
             List<Transform> newBranches = new List<Transform>();
 
             // for each unbranched trunk
-            Debug.Log(branchFrom.Count);
             foreach (Transform currTrunk in branchFrom)
             {
                 // create n branches from roughly top of trunk
@@ -70,10 +67,10 @@ public class TreeGenerator : MonoBehaviour
                 for (int i = 0; i < branchCount; i++)
                 {
                     // create, scale, rotate, and position new branch
-                    Transform currBranch = Instantiate(branchPrefab, trunk).transform;
-                    currBranch.localScale = new Vector3(1f / trunk.localScale.x, 1f / trunk.localScale.y, 1f / trunk.localScale.z) * 0.8f;
+                    Transform currBranch = Instantiate(branchPrefab, currTrunk).transform;
+                    currBranch.localScale = new Vector3(1f / currTrunk.localScale.x, 1f / currTrunk.localScale.y, 1f / currTrunk.localScale.z) * 0.8f;
                     currBranch.Rotate(new Vector3(45f, i * 360f / branchCount), Space.Self);
-                    currBranch.localPosition += Vector3.up * trunk.GetChild(0).localScale.y * 2f;
+                    currBranch.localPosition += Vector3.up * currTrunk.GetChild(0).localScale.y * 2f;
 
                     // add to list of future trunks
                     newBranches.Add(currBranch);

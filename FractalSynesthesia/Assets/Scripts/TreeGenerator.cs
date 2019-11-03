88149d3a5f0b522c428a5997a22a10f327f17f6e
Sam Layton <slayton@uccs.edu>
2019-11-03 09:44:27 -0700

    Rebuilt single generation to only manipulate primative cylinders rather than scaler-holders

--- FractalSynesthesia/Assets/Scripts/TreeGenerator.cs
+++ FractalSynesthesia/Assets/Scripts/TreeGenerator.cs
@@ -8,7 +8,9 @@ using UnityEngine;
 /// </summary>
 public class TreeGenerator : MonoBehaviour
 {
-    // branching support variables
+    // branching configuration variables
+    [Range(1, 50)]
+    [SerializeField] int maxGenerations = 10;       // max number of generations fractal will undergo before stopping
     [SerializeField] GameObject baseBranch;         // branch object spawned -- initialized to trunk placed in scene
 
     /// <summary>
@@ -29,7 +31,7 @@ public class TreeGenerator : MonoBehaviour
         // TODO: retrieve data from music analysis
 
         // generate fractal tree from trunk
-        GenerateTree(baseBranch.transform);
+        GenerateTree(baseBranch.transform, 1);
     }
 
     /// <summary>
@@ -38,19 +40,33 @@ public class TreeGenerator : MonoBehaviour
     /// </summary>
     /// <param name="trunk">current 'trunk' of tree to 
     /// grow branches from</param>
-    void GenerateTree(Transform trunk)
+    void GenerateTree(Transform trunk, int generation)
     {
+        // create list of future trunks to branch from
+        List<Transform> branchFrom = new List<Transform>();
+
         // create n branches from around top of trunk
         // NOTE: currently hard-set to 4 for easy testing
         int branchCount = 4;
         for (int i = 0; i < branchCount; i++)
         {
-            // create, position, rotate and scale branches of current trunk
-            GameObject currBranch = Instantiate(baseBranch, transform);
-            currBranch.transform.localPosition = 
-                Quaternion.Euler(trunk.localEulerAngles.x, trunk.localEulerAngles.y, trunk.localEulerAngles.z) * new Vector3(0, trunk.localScale.y * 2, 0);
-            currBranch.transform.Rotate(new Vector3(45, i * (360f / branchCount), 0));
-            currBranch.transform.localScale *= 0.8f;
+            // create, rotate, scale, and position branches of current trunk
+            Transform currBranch = Instantiate(trunk, transform).transform;
+            currBranch.Rotate(new Vector3(45, i * (360f / branchCount), 0));
+            currBranch.localScale *= 0.8f;
+            currBranch.localPosition = Quaternion.Euler(trunk.localEulerAngles.x / 2f, trunk.localEulerAngles.y / 2f, trunk.localEulerAngles.z / 2f) * 
+                (trunk.localPosition + (Vector3.up * trunk.localScale.y * 0.95f)) + 
+                Quaternion.Euler(currBranch.localEulerAngles.x, currBranch.localEulerAngles.y, currBranch.localEulerAngles.z) * (Vector3.up * currBranch.localScale.y);
+
+            // add branch to future trunks to branch from
+            branchFrom.Add(currBranch.transform);
+        }
+
+        // if tree hasn't reached max number of generations, continue with each new branch
+        if (generation < maxGenerations)
+        {
+            foreach (Transform branch in branchFrom)
+                GenerateTree(branch, generation + 1);
         }
     }
 }

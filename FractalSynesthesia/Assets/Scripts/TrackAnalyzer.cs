84d128045cab8192ea0d83a91ade8177266bbef7
Sam Layton <slayton@uccs.edu>
2019-11-03 13:25:56 -0700

    Created script to handle retrieving spectrum data from a given song

---
+++ FractalSynesthesia/Assets/Scripts/TrackAnalyzer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Pulls spectrum data from a given audio track
+/// </summary>
+[RequireComponent(typeof(AudioSource))]
+public class TrackAnalyzer : MonoBehaviour
+{
+
+}

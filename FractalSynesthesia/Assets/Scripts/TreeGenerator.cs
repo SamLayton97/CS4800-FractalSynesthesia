bf4c46df8bfd39c8c9a2165893e577991d3e568b
Sam Layton <slayton@uccs.edu>
2019-11-03 14:41:10 -0700

    Tree branches when appropriate

--- FractalSynesthesia/Assets/Scripts/TreeGenerator.cs
+++ FractalSynesthesia/Assets/Scripts/TreeGenerator.cs
@@ -16,6 +16,7 @@ public class TreeGenerator : MonoBehaviour
 
     // generation support variables
     Transform startingTrunk;                        // transform of initial branch object to build tree from
+    float growthRate = 1f;                          // rate at which branches grow before splitting -- entire tree should finish growing when song is over
 
     /// <summary>
     /// Read-access property returning number of generations
@@ -42,7 +43,7 @@ public class TreeGenerator : MonoBehaviour
     void Start()
     {
         // generate tree from starting branch
-        //StartCoroutine(GenerateTree(startingTrunk, maxGenerations));
+        StartCoroutine(GenerateTree(startingTrunk, maxGenerations));
     }
 
     /// <summary>
@@ -64,40 +65,50 @@ public class TreeGenerator : MonoBehaviour
         // initialize starting and target scale of trunk
         Vector3 startingScale = new Vector3(1, 0, 1);
         Vector3 targetScale = Vector3.one;
+        trunk.localScale = startingScale;
 
         // while generation has not exceeded limit
         while (generation <= maxGenerations)
         {
-            // initialize list storing branches created from this generation
+            // initialize list storing branches created by this generation
             List<Transform> newBranches = new List<Transform>();
 
             // for each unbranched trunk
             foreach (Transform currTrunk in toGrow)
             {
                 // TODO: scale branch up over time
-                currTrunk.localScale = Vector3.Lerp(startingScale, targetScale, 1);
+                currTrunk.localScale = Vector3.Lerp(startingScale, targetScale, currTrunk.localScale.y + Time.deltaTime * growthRate);
                 yield return new WaitForEndOfFrame();
 
-                // create n branches from roughly top of trunk
-                // NOTE: count hard set to 4 for testing
-                //int branchCount = 4;
-                //for (int i = 0; i < branchCount; i++)
-                //{
-                //    // create, scale, rotate, and position new branch
-                //    Transform currBranch = Instantiate(branchPrefab, currTrunk).transform;
-                //    currBranch.localScale = new Vector3(1f / currTrunk.localScale.x, 1f / currTrunk.localScale.y, 1f / currTrunk.localScale.z) * 0.7f;
-                //    currBranch.Rotate(new Vector3(35f, i * 360f / branchCount), Space.Self);
-                //    currBranch.localPosition += Vector3.up * currTrunk.GetChild(0).localScale.y * 2f;
+                // branch when trunk finishes growing
+                if (currTrunk.localScale.y >= targetScale.y)
+                {
+                    Debug.Log("branch!");
 
-                //    // add to list of future trunks
-                //    newBranches.Add(currBranch);
-                //}
+                    // create n branches from roughly top of trunk
+                    // NOTE: count hard set to 4 for testing
+                    int branchCount = 4;
+                    for (int i = 0; i < branchCount; i++)
+                    {
+                        // create, rotate, and position new branch
+                        Transform currBranch = Instantiate(branchPrefab, currTrunk).transform;
+                        currBranch.Rotate(new Vector3(35f, i * 360f / branchCount), Space.Self);
+                        currBranch.localPosition += Vector3.up * currTrunk.GetChild(0).localScale.y * 2f;
+
+                        // store starting and target scales of next branch generation
+                        startingScale = new Vector3(1f / currTrunk.localScale.x, 0, 1f / currTrunk.localScale.z) * 0.7f;
+                        targetScale = new Vector3(1f / currTrunk.localScale.x, 1f / currTrunk.localScale.y, 1f / currTrunk.localScale.z) * 0.7f;
+                        currBranch.localScale = startingScale;
+
+                        // add to list of future trunks
+                        newBranches.Add(currBranch);
+                    }
+
+                    // treat each new branch as a trunk for next generation
+                    toGrow = new List<Transform>(newBranches);
+                    generation++;
+                }
             }
-
-            // treat each new branch as a trunk for next generation
-            //toGrow = new List<Transform>(newBranches);
-            //generation++;
-            //yield return new WaitForEndOfFrame();
         }
     }
 }

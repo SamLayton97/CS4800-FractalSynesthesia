29810c696faf66a1edbe99fb27ed3526b5940472
Sam Layton <slayton@uccs.edu>
2019-11-02 23:34:50 -0600

    Added script to handle generation of fractal tree

---
+++ FractalSynesthesia/Assets/Scripts/TreeGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Generates a fractal tree using L-system and adjusted
+/// by values of music analyzer.
+/// </summary>
+public class TreeGenerator : MonoBehaviour
+{
+
+}

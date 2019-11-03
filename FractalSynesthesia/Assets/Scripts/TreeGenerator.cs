d2a4ea405990076809ec8c17516f7a7c1bac9df9
Sam Layton <slayton@uccs.edu>
2019-11-03 12:07:44 -0700

    Reformated method to be coroutine that performs a generation once per frame

--- FractalSynesthesia/Assets/Scripts/TreeGenerator.cs
+++ FractalSynesthesia/Assets/Scripts/TreeGenerator.cs
@@ -33,38 +33,57 @@ public class TreeGenerator : MonoBehaviour
         // TODO: retrieve data from music analysis
 
         // generate tree from starting branch
-        GenerateTree(startingTrunk, 0);
+        StartCoroutine(GenerateTree(startingTrunk, maxGenerations));
     }
 
     /// <summary>
-    /// Recusively generates a fractal tree from 
-    /// starting 'trunk'
+    /// Recursive coroutine which generates a fractal tree
+    /// over several frames until break case is reached
     /// </summary>
-    /// <param name="trunk">current 'trunk' of tree to 
-    /// grow branches from</param>
-    /// <param name="generation">current generation of fractal</param>
-    void GenerateTree(Transform trunk, int generation)
+    /// <param name="trunk">'trunk' of tree to branch from</param>
+    /// <param name="generation">number of generations before method stops</param>
+    /// <returns></returns>
+    IEnumerator GenerateTree(Transform trunk, int maxGenerations)
     {
-        // TODO: manipulate tree's color using music data
+        // initialize a generation counter
+        int generation = 1;
 
-        // if fractal hasn't exceeded generation limit
-        if (generation < maxGenerations)
+        // create list of trunks to branch from and append starting trunk
+        List<Transform> branchFrom = new List<Transform>();
+        branchFrom.Add(trunk);
+
+        // while generation has not exceeded limit
+        while (generation <= maxGenerations)
         {
-            // create n branches from roughly top of trunk
-            // NOTE: count hard set to 4 for testing
-            int branchCount = 4;
-            for (int i = 0; i < branchCount; i++)
-            {
-                // create and scale, rotate, and position new branch
-                Transform currBranch = Instantiate(branchPrefab, trunk).transform;
-                currBranch.localScale = new Vector3(1f / trunk.localScale.x, 1f / trunk.localScale.y, 1f / trunk.localScale.z) * 0.8f;
-                currBranch.Rotate(new Vector3(45f, i * 360f / branchCount), Space.Self);
-                currBranch.localPosition += Vector3.up * trunk.GetChild(0).localScale.y * 2f;
+            Debug.Log(generation);
 
-                // treat new branch as a trunk and continue generation
-                GenerateTree(currBranch, generation + 1);
+            // initialize list storing branches created from this generation
+            List<Transform> newBranches = new List<Transform>();
+
+            // for each unbranched trunk
+            Debug.Log(branchFrom.Count);
+            foreach (Transform currTrunk in branchFrom)
+            {
+                // create n branches from roughly top of trunk
+                // NOTE: count hard set to 4 for testing
+                int branchCount = 4;
+                for (int i = 0; i < branchCount; i++)
+                {
+                    // create, scale, rotate, and position new branch
+                    Transform currBranch = Instantiate(branchPrefab, trunk).transform;
+                    currBranch.localScale = new Vector3(1f / trunk.localScale.x, 1f / trunk.localScale.y, 1f / trunk.localScale.z) * 0.8f;
+                    currBranch.Rotate(new Vector3(45f, i * 360f / branchCount), Space.Self);
+                    currBranch.localPosition += Vector3.up * trunk.GetChild(0).localScale.y * 2f;
+
+                    // add to list of future trunks
+                    newBranches.Add(currBranch);
+                }
             }
-            
+
+            // treat each new branch as a trunk for next generation
+            branchFrom = new List<Transform>(newBranches);
+            generation++;
+            yield return new WaitForEndOfFrame();
         }
     }
 }

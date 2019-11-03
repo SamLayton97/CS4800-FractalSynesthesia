144886fb4e68a40390576c1c1d1f01e9a4968c58
Sam Layton <slayton@uccs.edu>
2019-11-03 13:32:30 -0700

    Audio source plays given track on initialization

--- FractalSynesthesia/Assets/Scripts/TrackAnalyzer.cs
+++ FractalSynesthesia/Assets/Scripts/TrackAnalyzer.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Pulls spectrum data from a given audio track
+/// Pulls spectrum data from a given audio track.
+/// Also handles playing of tracks.
 /// </summary>
 [RequireComponent(typeof(AudioSource))]
 public class TrackAnalyzer : MonoBehaviour
 {
+    // audio support variables
+    AudioSource myAudioSource;          // audio source to play tracks from
 
+    /// <summary>
+    /// Used for initialization
+    /// </summary>
+    void Awake()
+    {
+        // retrieve and initialize audio source
+        myAudioSource = GetComponent<AudioSource>();
+        if (!myAudioSource.clip)
+            myAudioSource.clip = Resources.Load<AudioClip>("Gorillaz - On Melancholy Hill");
+
+        // play track
+        myAudioSource.Play();
+    }
 }

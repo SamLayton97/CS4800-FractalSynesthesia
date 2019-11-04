62eabec08b55149e5d2c3ec9540b820d66e3b963
Sam Layton <slayton@uccs.edu>
2019-11-03 22:41:21 -0700

    Analyzer properly updates frequency bands each frame

--- FractalSynesthesia/Assets/Scripts/TrackAnalyzer.cs
+++ FractalSynesthesia/Assets/Scripts/TrackAnalyzer.cs
@@ -13,8 +13,9 @@ public class TrackAnalyzer : MonoBehaviour
     // audio support variables
     AudioSource myAudioSource;                      // audio source to play tracks from
     float[] samples = new float[1024];              // array of audio samples
-    float[] frequencyBands = new float[8];          // array storing amplitudes of simplified frequency bands
+    public float[] frequencyBands = new float[8];          // array storing amplitudes of simplified frequency bands
     int[] sampleCounts = new int[8];                // array storing amount of samples covered by each band
+    float frequencyMagnifier = 10f;                 // simple scalar to magnify small values of frequency bands
 
     // pseudo-singleton support
     static TrackAnalyzer instance;
@@ -87,11 +88,20 @@ public class TrackAnalyzer : MonoBehaviour
         // retrieve spectrum data of audio clip
         myAudioSource.GetSpectrumData(samples, 0, FFTWindow.BlackmanHarris);
 
-        // generate frequency bands of spectrum data
-        //for (int i = 0; i < frequencyBands.Length; i++)
-        //{
-
-        //}
+        // update frequency bands
+        // NOTE: calculations for each band based on: https://www.youtube.com/watch?v=mHk3ZiKNH48
+        int currSample = 0;
+        for (int i = 0; i < frequencyBands.Length; i++)
+        {
+            // calculate average value over sample range
+            float sampleSum = 0;
+            for (int j = 0; j < sampleCounts[i]; j++)
+            {
+                sampleSum += samples[currSample] * (currSample + 1);
+                currSample++;
+            }
+            frequencyBands[i] = sampleSum / currSample * frequencyMagnifier;
+        }
 
     }
 

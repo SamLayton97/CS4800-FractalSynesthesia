bd4fd600b0410935dea86e29c0f114d955561fa2
Sam Layton <slayton@uccs.edu>
2019-11-04 12:50:22 -0700

    Color shift rate is configurable

--- FractalSynesthesia/Assets/Scripts/ChangeBranchColor.cs
+++ FractalSynesthesia/Assets/Scripts/ChangeBranchColor.cs
@@ -11,10 +11,10 @@ public class ChangeBranchColor : MonoBehaviour
     // configuration variables
     [Range(0, 1)]
     [SerializeField] float adjustRate = 1f;     // rate at which branch shifts from one color to the next
-                                                // NOTE: 1 denotes branch shifts color instantaneously
+                                                // NOTE: 1 denotes near-instantaneous change in branch's color
 
     // color setting support variables
-    Color targetColor = Color.white;            // color branch shifts to over time
+    Color targetColor = Color.black;            // color branch shifts to over time
     Renderer branchRenderer;                    // reference to renderer of child branch primative -- used to control branch color
                                                 // NOTE: assumes child objects has Material component attached to it
     float shiftCounter = 0;
@@ -34,7 +34,7 @@ public class ChangeBranchColor : MonoBehaviour
     void Update()
     {
         // adjust material color over time
-        //shiftCounter += Time.deltaTime 
+        shiftCounter += Time.deltaTime * adjustRate;
         branchRenderer.material.color = Color.Lerp(branchRenderer.material.color, targetColor, shiftCounter);
     }
 }

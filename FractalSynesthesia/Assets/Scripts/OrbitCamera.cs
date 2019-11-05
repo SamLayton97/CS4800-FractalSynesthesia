b3675a4581de329b9db9815aadb9369f4224fda7
Sam Layton <slayton@uccs.edu>
2019-11-04 22:28:11 -0700

    Imported orbit camera from previous demo

---
+++ FractalSynesthesia/Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Enables user to rotate camera around an object
+/// using their mouse
+/// </summary>
+public class OrbitCamera : MonoBehaviour
+{
+    // public variables
+    public Transform targetTransform;       // transform of object to orbit about
+    public Vector3 lookOffset;              // initial offset to rotate camera (in degrees)
+    public float orbitSpeed = 5f;           // speed modifier to rotate camera at
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set camera to start facing object plus offset
+        transform.LookAt(targetTransform);
+        transform.Rotate(lookOffset);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if user is holding mouse button down
+        if (Input.GetMouseButton(0))
+        {
+            // temporarily hide mouse
+            Cursor.visible = false;
+
+            // rotate camera around object's y-axis using "Mouse X" input
+            transform.RotateAround(targetTransform.position, Vector3.down, Input.GetAxis("Mouse X") * Time.deltaTime * orbitSpeed * -1);
+        }
+        // otherwise, show cursor again
+        else
+            Cursor.visible = true;
+    }
+}

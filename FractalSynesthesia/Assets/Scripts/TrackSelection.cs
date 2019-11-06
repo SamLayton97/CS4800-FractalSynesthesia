5dddde1e157f99dcd4ebe64462b65bb39661cad6
Sam Layton <slayton@uccs.edu>
2019-11-06 16:34:46 -0700

    Moved selector to canvas of buttons -- allows for use of DontDestroyOnLoad()

--- FractalSynesthesia/Assets/Scripts/TrackSelection.cs
+++ FractalSynesthesia/Assets/Scripts/TrackSelection.cs
@@ -14,16 +14,45 @@ public class TrackSelection : MonoBehaviour
     [SerializeField] GameObject selectorButton;         // button prefab used to select particular track
     CanvasGroup myCanvasGroup;                          // selector's canvas group component -- used to control visibility
 
+    // singleton support
+    static TrackSelection instance;                     // instance of track selection singleton
+
+    /// <summary>
+    /// Read-access property returning instance of
+    /// track selection instance
+    /// </summary>
+    public static TrackSelection Instance
+    {
+        get { return instance; }
+    }
+
     /// <summary>
     /// Used for initialization
     /// </summary>
     void Awake()
     {
+        // destroy this instance if duplicate of singleton
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        // set this instance as singleton if not duplicate
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
         // retrieve and initialize canvas group to invisible
         myCanvasGroup = GetComponent<CanvasGroup>();
         myCanvasGroup.alpha = 0;
         myCanvasGroup.blocksRaycasts = false;
         myCanvasGroup.interactable = false;
+
+        // for each track in Resources
+        foreach (AudioClip unloadedTrack in Resources.LoadAll("", typeof(AudioClip)))
+        {
+            Debug.Log(unloadedTrack.name);
+        }
     }
 
     /// <summary>

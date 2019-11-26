5df00bcdf84cb23cc58044d680e2c38ac5ef5244
Sam Layton <slayton@uccs.edu>
2019-11-25 20:53:47 -0700

    Streaming assets supports both wav and ogg

--- FractalSynesthesia/Assets/Scripts/Track Loading and Selection/TrackSelectionManager.cs
+++ FractalSynesthesia/Assets/Scripts/Track Loading and Selection/TrackSelectionManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
@@ -78,9 +79,8 @@ public class TrackSelectionManager : MonoBehaviour
             currentTrack = Resources.Load<AudioClip>("DEFAULT John Coltrane - Giant Steps");    // if not set prior to launch, default clip
         InitializeTrack(currentTrack);
 
-        // load each track from StreamingAssets
+        // load custom tracks from StreamingAssets
         StartCoroutine(LoadCustomTracks());
-
     }
 
     /// <summary>
@@ -126,25 +126,42 @@ public class TrackSelectionManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Loads all .wav files from StreamingAssets,
+    /// Loads all supported audio files from StreamingAssets,
     /// returning them as an list of playable audioclips
     /// </summary>
     /// <returns>array of specific audio types</returns>
     public IEnumerator LoadCustomTracks()
     {
-        // load all .wav files from streaming assets
+        // load all supported audio files from streaming assets
         DirectoryInfo streamingAssets = new DirectoryInfo(Application.streamingAssetsPath);
-        FileInfo[] wavFiles = streamingAssets.GetFiles("*.wav");
+        FileInfo[] audioFiles = streamingAssets.GetFiles("*.*");
 
         // iterate over and convert each file
-        for (int i = 0; i < wavFiles.Length; i++)
+        for (int i = 0; i < audioFiles.Length; i++)
         {
             // construct uri
-            string filePath = wavFiles[i].FullName.ToString();
+            string filePath = audioFiles[i].FullName.ToString();
             string uri = string.Format("file://{0}", filePath);
 
+            Debug.Log(uri);
+
+            // determine audio type from file extension
+            AudioType type = AudioType.WAV;
+            switch (audioFiles[i].Extension)
+            {
+                // convert to one of supported audio types
+                case ".wav":
+                    break;
+                case ".ogg":
+                    type = AudioType.OGGVORBIS;
+                    break;
+                // if file isn't supported, skip it
+                default:
+                    continue;
+            }
+
             // create web request to particular audio file
-            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(uri, AudioType.WAV))
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(uri, type))
             {
                 yield return www.SendWebRequest();
 
@@ -156,7 +173,7 @@ public class TrackSelectionManager : MonoBehaviour
                 {
                     // initialize custom track
                     AudioClip newTrack = DownloadHandlerAudioClip.GetContent(www);
-                    newTrack.name = wavFiles[i].Name.ToString();
+                    newTrack.name = audioFiles[i].Name.ToString();
                     InitializeTrack(newTrack);
                 }
             }

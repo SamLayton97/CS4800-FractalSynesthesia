f7dd793887b4730ce192ce090758eb28bb0abf96
Sam Layton <slayton@uccs.edu>
2019-11-25 18:35:37 -0700

    Created custom coroutine that returns data

--- FractalSynesthesia/Assets/Scripts/Track Loading and Selection/CustomTrackLoader.cs
+++ FractalSynesthesia/Assets/Scripts/Track Loading and Selection/CustomTrackLoader.cs
@@ -30,34 +30,21 @@ public class CustomTrackLoader : MonoBehaviour
             string filePath = wavFiles[i].FullName.ToString();
             string uri = string.Format("file://{0}", filePath);
 
-            // find and convert audio clip using uri
-            yield return StartCoroutine(GetAudioClip(uri, AudioType.WAV));
-        }
-    }
-
-    /// <summary>
-    /// Retrieves and converts file into an audio
-    /// clip from StreamingAssets.
-    /// </summary>
-    /// <param name="uri">uniform resource indentifier of audio file</param>
-    /// <param name="type">Unity-supported audio file type to convert as</param>
-    /// <returns></returns>
-    IEnumerator GetAudioClip(string uri, AudioType type)
-    {
-        // create web request to particular audio file
-        using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(uri, type))
-        {
-            yield return www.SendWebRequest();
-
-            // catch and log errors
-            if (www.isNetworkError)
-                Debug.LogError(www.error);
-            // otherwise (no problems getting track)
-            else
+            // create web request to particular audio file
+            using (UnityWebRequest www = UnityWebRequestMultimedia.GetAudioClip(uri, AudioType.WAV))
             {
-                // TODO: append track to list of custom songs
-                AudioClip newTrack = DownloadHandlerAudioClip.GetContent(www);
+                yield return www.SendWebRequest();
 
+                // catch and log errors
+                if (www.isNetworkError)
+                    Debug.LogError(www.error);
+                // otherwise (no problems getting track)
+                else
+                {
+                    // TODO: append track to list of custom songs
+                    AudioClip newTrack = DownloadHandlerAudioClip.GetContent(www);
+
+                }
             }
         }
     }
---
+++ FractalSynesthesia/Assets/Scripts/Utility/CoroutineWithData.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Parent class simulating a coroutine that returns
+/// data once finished
+/// Credit to Ted Bigham
+/// https://answers.unity.com/questions/24640/how-do-i-return-a-value-from-a-coroutine.html
+/// </summary>
+public class CoroutineWithData
+{
+    // return value support variables
+    object data;
+    IEnumerator target;
+
+    /// <summary>
+    /// Property with public read-access and private
+    /// write-access returning coroutine to perform
+    /// </summary>
+    public Coroutine Action
+    {
+        get;
+        private set;
+    }
+
+    /// <summary>
+    /// Constructor for a coroutine that returns data
+    /// </summary>
+    /// <param name="owner">MonoBehavior object that initates coroutine</param>
+    /// <param name="target">coroutine action to perform</param>
+    public CoroutineWithData(MonoBehaviour owner, IEnumerator target)
+    {
+        // set target and run coroutine
+        this.target = target;
+        this.Action = owner.StartCoroutine(target);
+    }
+
+    /// <summary>
+    /// Runs coroutine, returning result once finished
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Run()
+    {
+        // once coroutine has ended, return data
+        while (target.MoveNext())
+        {
+            data = target.Current;
+            yield return data;
+        }
+    }
+}

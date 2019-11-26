270565792bffda1b9ce3297a7ad596c8a67fa43a
Sam Layton <slayton@uccs.edu>
2019-11-26 16:18:04 -0700

    Removed unused utility class; created script to control wind on grass

---
+++ FractalSynesthesia/Assets/Scripts/Environment/WindChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Control scale, speed, tint of wind over
+/// grass according to track analysis data.
+/// </summary>
+public class WindChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

4a34a50e85c767594bcac1f4543c2cc5f65ed4ee
Sam Layton <slayton@uccs.edu>
2019-12-28 21:15:06 -0700

    Created script to process autoplay toggle and starting of subsequent songs

---
+++ FractalSynesthesia/Assets/Scripts/Track Loading and Selection/AutoPlayManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages automatic starting of next song in 
+/// track list when current one ends
+/// </summary>
+public class AutoPlayManager : MonoBehaviour
+{
+    
+}
